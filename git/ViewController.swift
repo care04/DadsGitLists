5c11f6592f513ee4c797facf8df3616e79b6756a
Care <careluanne@gmail.com>
2021-07-22 13:06:35 -0400

    added a crib var

--- git/ViewController.swift
+++ git/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var crib = "C-R-I-B"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
